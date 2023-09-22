2dacfed2b45b74370cce48963c5df691cd5f9392
haobayuxi <whaohit@gmail.com>
2023-09-22 22:39:02 +0800

    update

--- core/server/server.cc
+++ core/server/server.cc
@@ -3,11 +3,18 @@
 
 #include <stdio.h>
 
+int rlock[10] = {0};
+
+int64_t rwlock_test() {}
+
+int64_t lock_unlock() {}
+
 int main(int argc, char* argv[]) {
   auto times = 10000000;
   auto start = rdtsc_s();
   // start
-  for (auto i = 0; i < times; i++) {
+  for (auto i = 0; i < 10; i++) {
+    printf("%d ", rlock[i]);
   }
   // end
   auto end = rdtsc_e();
