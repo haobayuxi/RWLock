2c2fcc77cdc1fd020dd96581acd08871f382a7a6
haobayuxi <whaohit@gmail.com>
2023-09-22 21:54:03 +0800

    test rtdsc

--- core/server/server.cc
+++ core/server/server.cc
@@ -1,6 +1,17 @@
 
 #include "server.h"
 
+#include <intrin.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[]) { printf("test\n"); }
+#pragma intrinsic(__rdtsc)
+
+int main(int argc, char* argv[]) {
+  auto times = 10000000;
+  // start
+  for (auto i = 0; i < times; i++) {
+  }
+  // end
+  auto t = rtdsc();
+  printf("test %ld\n", t);
+}
