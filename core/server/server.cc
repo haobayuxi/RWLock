1f3b554b07c2b849295052dc32a795d94cbb0da3
haobayuxi <whaohit@gmail.com>
2023-09-24 12:31:08 +0800

    update

--- core/server/server.cc
+++ core/server/server.cc
@@ -2,8 +2,19 @@
 #include "server.h"
 
 #include <stdio.h>
+#include <time.h>
 
 int rlock[10] = {0};
+
+long long get_clock_sys_time_us() {
+  struct timespec tp;
+  long long time_us = 0;
+
+  clock_gettime(CLOCK_MONOTONIC, &tp);
+  time_us = (long long)tp.tv_sec * 1000000 + tp.tv_nsec / 1000;
+
+  return time_us;
+}
 
 int64_t rwlock_test() {}
 
@@ -11,12 +22,12 @@ int64_t lock_unlock() {}
 
 int main(int argc, char* argv[]) {
   auto times = 10000000;
-  auto start = rdtsc_s();
+  auto start = get_clock_sys_time_us();
   // start
   for (auto i = 0; i < 10; i++) {
-    printf("%d ", rlock[i]);
+    // printf("%d ", rlock[i]);
   }
   // end
-  auto end = rdtsc_e();
+  auto end = get_clock_sys_time_us();
   printf("test %ld\n", end - start);
 }
