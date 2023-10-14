db3ac5a28f42af10e5730d418097bd001930a27a
haobayuxi <whaohit@gmail.com>
2023-10-14 20:31:03 +0800

    update

--- compute_pool/worker/handler.cc
+++ compute_pool/worker/handler.cc
@@ -190,7 +190,7 @@ void Handler::GenThreads(std::string bench_name) {
   }
 
   RDMA_LOG(INFO) << "committed " << total << "time " << msr_sec
-                 << " persecond=" << ((double)total) / msr_sec;
+                 << " persecond=" << (total * 1.0) / msr_sec;
 
   delete[] param_arr;
   delete global_rdma_region;
