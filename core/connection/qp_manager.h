db2726e1a9f1aa8fcf3b66ab95bd46ad7a355178
haobayuxi <whaohit@gmail.com>
2023-10-14 14:42:55 +0800

    update

--- core/connection/qp_manager.h
+++ core/connection/qp_manager.h
@@ -7,8 +7,30 @@ class QPManager {
   QPManager(t_id_t golbal_tid) : global_tid(global_tid) {}
   void BuildQPConnection(MetaManager* meta_man);
 
+  ALWAYS_INLINE
+  RCQP* GetRemoteDataQPWithNodeID(const node_id_t node_id) const {
+    return data_qps[node_id];
+  }
+
+  ALWAYS_INLINE
+  void GetRemoteDataQPsWithNodeIDs(const std::vector<node_id_t>* node_ids,
+                                   std::vector<RCQP*>& qps) {
+    for (node_id_t node_id : *node_ids) {
+      RCQP* qp = data_qps[node_id];
+      if (qp) {
+        qps.push_back(qp);
+      }
+    }
+  }
+
+  ALWAYS_INLINE
+  RCQP* GetRemoteLogQPWithNodeID(const node_id_t node_id) const {
+    return log_qps[node_id];
+  }
+
   RCQP* data_qps[MAX_REMOTE_NODE_NUM]{nullptr};
 
  private:
+  RCQP* log_qps[MAX_REMOTE_NODE_NUM]{nullptr};
   t_id_t global_tid;
 };
