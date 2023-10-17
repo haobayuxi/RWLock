8e7ec7ffd4b591a1254b7e04a3ccdb839fd2bee2
haobayuxi <whaohit@gmail.com>
2023-10-17 15:59:13 +0800

    update

---
+++ standalone/server/server.cc
@@ -0,0 +1,42 @@
+
+#include "server.h"
+
+// using namespace std;
+
+const uint64_t mem_size = 1024 * 1024 * 1024;
+char* test_memory;
+
+// test for random cas and read in memory
+void Server::run_test() {
+  // init memory
+  test_memory = (char*)malloc(mem_size);
+  memset(test_memory, 0, mem_size);
+
+  //   random cas
+}
+
+void Server::gen_threads(int thread_num) {
+  auto thread_arr = new std::thread[thread_num];
+  for (int i = 0; i < thread_num; i++) {
+    // thread_arr[i] =
+    //     std::thread(run_thread, &param_arr[i], tatp_client, tpcc_client);
+    thread_arr[i] = std::thread(run_test);
+    /* Pin thread i to hardware thread i */
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(i, &cpuset);
+    int rc = pthread_setaffinity_np(thread_arr[i].native_handle(),
+                                    sizeof(cpu_set_t), &cpuset);
+    if (rc != 0) {
+      std::cout << "Error calling pthread_setaffinity_np: " << rc;
+    }
+  }
+
+  for (int i = 0; i < thread_num; i++) {
+    if (thread_arr[i].joinable()) {
+      thread_arr[i].join();
+    }
+  }
+}
+
+int main(int argc, char* argv[]) { return 0; }
---
+++ standalone/server/server.h
@@ -0,0 +1,34 @@
+#define _GNU_SOURCE
+#include <pthread.h>
+#include <sched.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <thread>
+
+#define ALWAYS_INLINE inline __attribute__((always_inline))
+
+class Server {
+ public:
+  void run_test();
+  void gen_threads(int thread_num);
+
+ private:
+  int thread_num;
+};
+
+ALWAYS_INLINE
+long long get_clock_sys_time_us() {
+  struct timespec tp;
+  long long time_us = 0;
+
+  clock_gettime(CLOCK_MONOTONIC, &tp);
+  time_us = (long long)tp.tv_sec * 1000000 + tp.tv_nsec / 1000;
+
+  return time_us;
+}
