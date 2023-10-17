32d89af7f879463eaa8c1b966095e9c2292013eb
haobayuxi <whaohit@gmail.com>
2023-10-17 19:17:27 +0800

    update

--- standalone/server/server.cc
+++ standalone/server/server.cc
@@ -13,10 +13,10 @@ uint64_t commits[100];
 void run_test(int thread_id, int thread_num) {
   //   random cas
   auto offset = sizeof(uint64_t) * thread_id;
-  bool exp = 0;
+  uint64_t exp = 0;
   for (int i = 0; i < 1000; i++) {
     auto ptr = (std::atomic<uint64_t>*)(test_memory + offset);
-    ptr->compare_exchange_strong(0, 1);
+    ptr->compare_exchange_strong(&exp, 1);
     offset = (offset + thread_num) % mem_size;
   }
   std::cout << " thread " << thread_id << std::endl;
