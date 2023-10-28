3f71f03e96e06853bd4684366e75bdc77ead8d21
haobayuxi <whaohit@gmail.com>
2023-10-28 20:40:28 +0800

    update

--- core/scheduler/corotine_scheduler.cc
+++ core/scheduler/corotine_scheduler.cc
@@ -7,7 +7,6 @@
 
 #include "util/debug.h"
 
-ALWAYS_INLINE
 void CoroutineScheduler::PollRegularCompletion() {
   for (auto it = pending_qps.begin(); it != pending_qps.end();) {
     RCQP* qp = *it;
@@ -39,7 +38,7 @@ void CoroutineScheduler::PollRegularCompletion() {
     it = pending_qps.erase(it);
   }
 }
-ALWAYS_INLINE
+
 void CoroutineScheduler::PollLogCompletion() {
   for (auto it = pending_log_qps.begin(); it != pending_log_qps.end();) {
     RCQP* qp = *it;
@@ -68,12 +67,12 @@ void CoroutineScheduler::PollLogCompletion() {
     it = pending_log_qps.erase(it);
   }
 }
-ALWAYS_INLINE
+
 void CoroutineScheduler::PollCompletion() {
   PollRegularCompletion();
   PollLogCompletion();
 }
-ALWAYS_INLINE
+
 bool CoroutineScheduler::CheckLogAck(coro_id_t c_id) {
   if (pending_log_counts[c_id] == 0) {
     return true;
