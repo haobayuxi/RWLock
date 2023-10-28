6c4fa7431d5cfde0186c00be441b8d475a6b754c
haobayuxi <whaohit@gmail.com>
2023-10-28 15:55:13 +0800

    update

--- workload/micro/micro_txn.cc
+++ workload/micro/micro_txn.cc
@@ -141,6 +141,8 @@ bool TxReadOnly(ZipfGen* zipf_gen, uint64_t* seed, coro_yield_t& yield,
     // TLOG(DBG, thread_gid) << "tx " << tx_id << " aborts after exe";
     return false;
   }
+  auto end_time = get_clock_sys_time_us();
+  RDMA_LOG(INFO) << "cost time =" << end_time - dtx->start_time;
 
   bool commit_status = dtx->TxCommit(yield);
   // TLOG(DBG, thread_gid) << "tx " << tx_id << " commit? " << commit_status;
