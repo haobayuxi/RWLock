2b48acf899ddfa02f5dcaa29915f324391f17ade
haobayuxi <whaohit@gmail.com>
2023-10-28 23:40:34 +0800

    update

--- core/dtx/dtx.cc
+++ core/dtx/dtx.cc
@@ -128,7 +128,6 @@ bool DTX::TxCommit(coro_yield_t& yield) {
     if (!Validate(yield)) {
       goto ABORT;
     }
-    RDMA_LOG(INFO) << "occ commit";
     // Next step. If read-write txns, we need to commit the updates to remote
     // replicas
     if (!read_write_set.empty()) {
