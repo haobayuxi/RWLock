f38814be06a8920b1784615b5c964a397ed279e1
haobayuxi <whaohit@gmail.com>
2023-10-28 16:04:36 +0800

    update

--- core/dtx/oocc.cc
+++ core/dtx/oocc.cc
@@ -135,7 +135,7 @@ bool DTX::CasWriteLockAndRead(coro_yield_t& yield) {
 bool DTX::OOCCCheck(coro_yield_t& yield, bool read_only) {
   //   auto end = get_clock_sys_time_us();
   if (!CheckDirectRO()) return false;
-  if (!CheckCAS()) return false;
+  //   if (!CheckCAS()) return false;
   if (!CheckHash()) return false;
 
   //   auto end1 = get_clock_sys_time_us();
