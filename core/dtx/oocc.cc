976c60985653c2cf8ed66821a4468abbe4d18ebc
haobayuxi <whaohit@gmail.com>
2023-10-29 13:04:21 +0800

    update

--- core/dtx/oocc.cc
+++ core/dtx/oocc.cc
@@ -180,7 +180,10 @@ bool DTX::CheckDirectRO() {
     // auto* it = res.item->item_ptr.get();
     res.item->is_fetched = true;
     auto* fetched_item = (DataItem*)res.buf;
-    // if (fetched_item->lock != 0) return false;
+    if (fetched_item->lock != 0) {
+      RDMA_LOG(INFO) << "lock";
+      return false;
+    }
   }
   //   pending_direct_ro.clear();
   return true;
